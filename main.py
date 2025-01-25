efb652446a58fdefbf170b44d660adaf8dd94c50
SudeepPasula <152053539+SudeepPasula@users.noreply.github.com>
2025-01-25 17:51:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def plot_stock_price(ticker):
     data = yf.Ticker(ticker).history(period = '1y')
     plt.figure(figsize = (10, 5))
     plt.plot(data.index, data.Close)
-    plt.title('{ticker} Stock Price Over Last Year')
+    plt.title(f'{ticker} Stock Price Over Last Year')
     plt.xlabel('Date')
     plt.ylabel('Stock Price ($)')
     plt.grid(True)
